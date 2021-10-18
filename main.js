4782a6d4f0f4be4e82f0b09bbac07c72402bb086
Somdotta07 <jhilick.s@gmail.com>
2021-10-18 14:54:27 +0200

    Add js for storage
    Add variables
    Add local storage

---
+++ main.js
@@ -0,0 +1,5 @@
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookList = document.getElementById('book-list');
+const button = document.getElementById('btn');
+
