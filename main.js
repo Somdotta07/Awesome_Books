e0473fba66932753b0f28f11beeef8e5b728356d
Somdotta07 <jhilick.s@gmail.com>
2021-10-18 14:56:06 +0200

    Update main.js
    Add variables
    Add local storage

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
+/* eslint-disable linebreak-style */
+//
+
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookList = document.getElementById('book-list');
 const button = document.getElementById('btn');
+const books = [];
+let id;
 
+class book {
+    id = this.id;
+
+    name = this.name;
+
+    author = this.author;
+}
+
+// Local Storage
+if (localStorage.getItem('bookList') === null) {
+  localStorage.setItem('bookList', []);
+}
+
+if (localStorage.getItem('id') === null) {
+  localStorage.setItem('id', JSON.stringify(0));
+  id = JSON.parse(localStorage.getItem('id'));
+}
